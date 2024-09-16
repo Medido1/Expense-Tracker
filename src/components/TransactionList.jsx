ab938233307f2540e769a88aa8935bdf10506237
Medido1 <medi1222@gmail.com>
2024-09-16 21:41:16 +0100

    Finish Project

---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,23 @@
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+import Transaction from "./Transaction";
+
+function TransactionList() {
+  const {transactions} = useContext(GlobalContext);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul id="list" className="list">
+        {transactions.map((transaction) => {
+          return <Transaction key={transaction.id} transaction={transaction}/>
+        })}
+        {/* <li className="minus">
+          Cash <span>-$400</span><button className="delete-btn">x</button>
+        </li>  */}
+      </ul>
+    </>
+  )
+}
+
+export default TransactionList;
